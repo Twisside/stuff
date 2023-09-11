25659d51d02fcc53984a5289cc0179cec2c12f21
Twisside <136237170+Twisside@users.noreply.github.com>
2023-09-11 14:10:50 +0300

    Car.cs

--- Car.cs
+++ Car.cs
@@ -1,15 +1,26 @@
-﻿
-public class Car
+namespace Class_exampl
 {
-    private string brand = "mazda";
-    private string model = "mx-5";
-    private int year = 1993;
-    private string colour = "red";
+    public class Car
+    {
+        private static string brand = "mazda";
+        private static string model = "mx-5";
+        private static int year = 1993;
+        private static string colour = "red";
 
-    private string licence_plate = "mzdfl";
-    private int hp = 180;
-    private int milliadge = 15000;
-    private int transmission = 0; // 0 - mechanical, 1 - automatic
+        private string licence_plate = "mzdfl";
+        private int hp = 180;
+        private int milliadge = 15000;
+        private int transmission = 0; // 0 - mechanical, 1 - automatic
+        private static float speed = 0;
 
+        public void Details()
+        {
+            Console.WriteLine(Car.brand, Car.model, Car.year, Car.colour);
+        }
 
+        public void Motion(float speed)
+        {
+            Car.speed = Convert.ToSingle(Console.ReadLine());
+        }
+    }
 }
