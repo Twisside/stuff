d5aec1e991e474dd65ecf6a0da68ed6005ca4b61
Twisside <136237170+Twisside@users.noreply.github.com>
2023-09-11 13:29:34 +0300

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,15 @@
+﻿
+public class Car
+{
+    private string brand = "mazda";
+    private string model = "mx-5";
+    private int year = 1993;
+    private string colour = "red";
+
+    private string licence_plate = "mzdfl";
+    private int hp = 180;
+    private int milliadge = 15000;
+    private int transmission = 0; // 0 - mechanical, 1 - automatic
+
+
+}
